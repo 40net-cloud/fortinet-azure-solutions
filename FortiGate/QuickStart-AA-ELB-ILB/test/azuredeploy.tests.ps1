1b139c0d37a662dcd7af577c0d2669d5415b490b
Joeri Van Hoof <jvh@jvh.be>
2019-03-18 14:21:57 +0100

    Initial commit

---
+++ FortiGate/QuickStart-AA-ELB-ILB/test/azuredeploy.tests.ps1
@@ -0,0 +1,161 @@
+#Requires -Modules Pester
+<#
+.SYNOPSIS
+    Tests a specific ARM template
+.EXAMPLE
+    Invoke-Pester 
+.NOTES
+    This file has been created as an example of using Pester to evaluate ARM templates
+#>
+
+Function random-password ($length = 15) {
+    $punc = 46..46
+    $digits = 48..57
+    $letters = 65..90 + 97..122
+
+    # Thanks to
+    # https://blogs.technet.com/b/heyscriptingguy/archive/2012/01/07/use-pow
+    $password = get-random -count $length `
+        -input ($punc + $digits + $letters) |
+        % -begin { $aa = $null } `
+        -process {$aa += [char]$_} `
+        -end {$aa}
+
+    return $password
+}
+
+$templateName = "QuickStart-AA-ELB-ILB"
+$sourcePath = "$env:BUILD_SOURCESDIRECTORY\FortiGate\$templateName"
+$scriptPath = "$env:BUILD_SOURCESDIRECTORY\FortiGate\$templateName\test"
+$templateFileName = "azuredeploy.json"
+$templateFileLocation = "$sourcePath\$templateFileName"
+$templateMetadataFileName = "metadata.json"
+$templateMetadataFileLocation = "$sourcePath\$templateMetadataFileName"
+$templateParameterFileName = "azuredeploy.parameters.json"
+$templateParameterFileLocation = "$sourcePath\$templateParameterFileName" 
+
+Describe 'ARM Templates Test : Validation & Test Deployment' {
+    Context 'Template Validation' {
+        It 'Has a JSON template' {        
+            $templateFileLocation | Should Exist
+        }
+        
+        It 'Has a parameters file' {        
+            $templateParameterFileLocation | Should Exist
+        }
+		
+        It 'Has a metadata file' {        
+            $templateMetadataFileLocation | Should Exist
+        }
+
+        It 'Converts from JSON and has the expected properties' {
+            $expectedProperties = '$schema',
+            'contentVersion',
+            'outputs',
+            'parameters',
+            'resources',                                
+            'variables'
+            $templateProperties = (get-content $templateFileLocation | ConvertFrom-Json -ErrorAction SilentlyContinue) | Get-Member -MemberType NoteProperty | % Name
+            $templateProperties | Should Be $expectedProperties
+        }
+        
+        It 'Creates the expected Azure resources' {
+            $expectedResources = 'Microsoft.Compute/availabilitySets',
+            'Microsoft.Network/virtualNetworks',
+            'Microsoft.Network/loadBalancers',
+            'Microsoft.Network/routeTables',
+            'Microsoft.Network/networkSecurityGroups',
+            'Microsoft.Network/publicIPAddresses',
+            'Microsoft.Network/publicIPAddresses',
+            'Microsoft.Network/loadBalancers',
+            'Microsoft.Network/networkInterfaces',
+            'Microsoft.Network/networkInterfaces',
+            'Microsoft.Network/networkInterfaces',
+            'Microsoft.Network/networkInterfaces',
+            'Microsoft.Compute/virtualMachines',
+            'Microsoft.Compute/virtualMachines'
+            $templateResources = (get-content $templateFileLocation | ConvertFrom-Json -ErrorAction SilentlyContinue).Resources.type
+            $templateResources | Should Be $expectedResources
+        }
+        
+        It 'Contains the expected parameters' {
+            $expectedTemplateParameters = 'adminUsername',
+            'adminPassword',
+            'FortiGateNamePrefix',
+            'FortiGateImageSku',
+            'FortiGateImageVersion',
+            'instanceType',
+            'publicIPNewOrExisting',
+            'publicIP2NewOrExisting',
+            'publicIPAddressName',
+            'publicIPAddressResourceGroup',
+            'publicIP2AddressName',
+            'publicIP2AddressResourceGroup',
+            'publicIPAddressType',
+            'vnetNewOrExisting',
+            'vnetName',
+            'vnetResourceGroup',
+            'vnetAddressPrefix',
+            'subnet1Name',
+            'subnet1Prefix',
+            'subnet2Name',
+            'subnet2Prefix',
+            'subnet3Name',
+            'subnet3Prefix',
+            'FortinetTags'
+
+            $templateParameters = (get-content $templateFileLocation | ConvertFrom-Json -ErrorAction SilentlyContinue).Parameters | Get-Member -MemberType NoteProperty | % Name
+            $templateParameters | Should Be $expectedTemplateParameters
+        }
+
+    }
+
+    Context 'Template Test Deployment' {
+
+        # Basic Variables
+        $testsRandom = Get-Random 10001
+        $testsPrefix = "FORTIQA"
+        $testsResourceGroupName = "FORTIQA-$testsRandom-$templateName"
+        $testsAdminPassword = $testsResourceGroupName | ConvertTo-SecureString -AsPlainText -Force
+        $testsResourceGroupLocation = "East US2"
+
+        # List of all scripts + parameter files
+        $testsTemplateList = @()
+        ## dummy parameter file to test default parameters
+        $testsTemplateList += , @("azuredeploy.json", "azuredeploy.parameters.json")
+
+        # Set working directory & create resource group
+        Set-Location $sourcePath
+        New-AzureRmResourceGroup -Name $testsResourceGroupName -Location "$testsResourceGroupLocation"
+
+        # Validate all ARM templates one by one
+        $testsErrorFound = $false
+
+        $params = @{ 'ResourceGroupName'=$testsResourceGroupName;
+                     'TemplateFile'='azuredeploy.json';
+                     'TemplateParameterFile'='azuredeploy.parameters.json';
+                     'adminPassword'=$testsAdminPassword;
+                     'FortiGateNamePrefix'=$testsPrefix;
+                     'publicIPAddressName'=$testsPrefix + '-PIP';
+                     'publicIPAddress2Name'=$testsPrefix + '-PIP2';
+                     'vnetName'=$testsPrefix + '-VNET';
+                     'vnetResourceGroup'=$testsResourceGroupName;
+                    }
+
+        It "Test Deployment of ARM template $testsTemplateFile with parameter file $testsTemplateParemeterFile" {
+            (Test-AzureRmResourceGroupDeployment @params ).Count | Should not BeGreaterThan 0
+        }
+        It "Deployment of ARM template $testsTemplateFile with parameter file $testsTemplateParemeterFile" {
+            $resultDeployment = New-AzureRmResourceGroupDeployment @params
+            Write-Host ($resultDeployment | Format-Table | Out-String)
+            Write-Host "Deployment state: $resultDeployment.ProvisioningState"
+            $resultDeployment.ProvisioningState | Should Be "Succeeded"
+        }
+        It "Deployment in Azure validation" {
+            $result = Get-AzureRmVM | Where-Object { $_.Name -like "$testsPrefix*" } 
+            Write-Host ($result | Format-Table | Out-String)
+            $result | Should Not Be $null
+        }
+        Remove-AzureRmResourceGroup -Name $testsResourceGroupName -Force
+    }
+}
