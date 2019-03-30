b3e12eb1e7683c2279a0fdfacad8c66964ab3646
Joeri Van Hoof <jvh@jvh.be>
2019-03-30 09:48:46 +0100

    Updated tests

--- FortiGate/QuickStart-AP/test/azuredeploy.tests.ps1
+++ FortiGate/QuickStart-AP/test/azuredeploy.tests.ps1
@@ -170,13 +170,14 @@ Describe 'ARM Templates Test : Validation & Test Deployment' {
         8443, 22 | Foreach-Object {
             it "Port [$_] is listening" {
                 $result = Get-AzureRmPublicIpAddress -Name $params['publicIPName'] -ResourceGroupName $params['ResourceGroupName']
-                $portListening = (Test-NetConnection -Port $_ -ComputerName $result).TcpTestSucceeded
+                Write-Host ("Checking PIP: $result)
+                $portListening = (Test-NetConnection -Port $_ -ComputerName $result.IpAddress).TcpTestSucceeded
                 $portListening | Should -Be $true
                 $result = Get-AzureRmPublicIpAddress -Name $params['publicIP2Name'] -ResourceGroupName $params['ResourceGroupName']
-                $portListening = (Test-NetConnection -Port $_ -ComputerName $result).TcpTestSucceeded
+                $portListening = (Test-NetConnection -Port $_ -ComputerName $result.IpAddress).TcpTestSucceeded
                 $portListening | Should -Be $true
                 $result = Get-AzureRmPublicIpAddress -Name $params['publicIP3Name'] -ResourceGroupName $params['ResourceGroupName']
-                $portListening = (Test-NetConnection -Port $_ -ComputerName $result).TcpTestSucceeded
+                $portListening = (Test-NetConnection -Port $_ -ComputerName $result.IpAddress).TcpTestSucceeded
                 $portListening | Should -Be $true
             }
         }
