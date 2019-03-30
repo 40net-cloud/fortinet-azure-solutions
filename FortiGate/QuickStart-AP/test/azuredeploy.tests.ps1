c74e879c0f0ec779ceca72c983312e4efcec65d8
Joeri Van Hoof <jvh@jvh.be>
2019-03-30 09:53:11 +0100

    Typo in test

--- FortiGate/QuickStart-AP/test/azuredeploy.tests.ps1
+++ FortiGate/QuickStart-AP/test/azuredeploy.tests.ps1
@@ -170,7 +170,7 @@ Describe 'ARM Templates Test : Validation & Test Deployment' {
         8443, 22 | Foreach-Object {
             it "Port [$_] is listening" {
                 $result = Get-AzureRmPublicIpAddress -Name $params['publicIPName'] -ResourceGroupName $params['ResourceGroupName']
-                Write-Host ("Checking PIP: $result)
+                Write-Host ("Checking PIP: " + $result.IpAddress)
                 $portListening = (Test-NetConnection -Port $_ -ComputerName $result.IpAddress).TcpTestSucceeded
                 $portListening | Should -Be $true
                 $result = Get-AzureRmPublicIpAddress -Name $params['publicIP2Name'] -ResourceGroupName $params['ResourceGroupName']
