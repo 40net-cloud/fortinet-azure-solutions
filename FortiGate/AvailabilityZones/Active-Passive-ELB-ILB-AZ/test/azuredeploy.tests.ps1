36917f7c5a06c8ad9de10e09f3bcb31257477017
Joeri Van Hoof <jvh@jvh.be>
2019-05-20 09:10:22 +0200

    Updated rg parameter in test

--- FortiGate/AvailabilityZones/Active-Passive-ELB-ILB-AZ/test/azuredeploy.tests.ps1
+++ FortiGate/AvailabilityZones/Active-Passive-ELB-ILB-AZ/test/azuredeploy.tests.ps1
@@ -161,10 +161,10 @@ Describe 'ARM Templates Test : Validation & Test Deployment' {
 
         8443, 22 | Foreach-Object {
             it "Port [$_] is listening" {
-                $result = Get-AzureRmPublicIpAddress -Name $publicIP2Name -ResourceGroupName $params['ResourceGroupName']
+                $result = Get-AzureRmPublicIpAddress -Name $publicIP2Name -ResourceGroupName $testsResourceGroupName
                 $portListening = (Test-NetConnection -Port $_ -ComputerName $result.IpAddress).TcpTestSucceeded
                 $portListening | Should -Be $true
-                $result = Get-AzureRmPublicIpAddress -Name $publicIP3Name -ResourceGroupName $params['ResourceGroupName']
+                $result = Get-AzureRmPublicIpAddress -Name $publicIP3Name -ResourceGroupName $testsResourceGroupName
                 $portListening = (Test-NetConnection -Port $_ -ComputerName $result.IpAddress).TcpTestSucceeded
                 $portListening | Should -Be $true
             }
