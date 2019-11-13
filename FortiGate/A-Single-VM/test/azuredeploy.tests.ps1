a7e5350ef7789efd3fdab4208abe4482a0c08003
Joeri Van Hoof <jvanhoof@fortinet.com>
2019-11-13 17:40:34 +0100

    Update tests public ip

--- FortiGate/A-Single-VM/test/azuredeploy.tests.ps1
+++ FortiGate/A-Single-VM/test/azuredeploy.tests.ps1
@@ -138,10 +138,7 @@ Describe 'ARM Templates Test : Validation & Test Deployment' {
 
         443, 22 | Foreach-Object {
             it "Port [$_] is listening" {
-                $result = Get-AzureRmPublicIpAddress -Name $publicIP2Name -ResourceGroupName $testsResourceGroupName
-                $portListening = (Test-NetConnection -Port $_ -ComputerName $result.IpAddress).TcpTestSucceeded
-                $portListening | Should -Be $true
-                $result = Get-AzureRmPublicIpAddress -Name $publicIP3Name -ResourceGroupName $testsResourceGroupName
+                $result = Get-AzureRmPublicIpAddress -Name $publicIPName -ResourceGroupName $testsResourceGroupName
                 $portListening = (Test-NetConnection -Port $_ -ComputerName $result.IpAddress).TcpTestSucceeded
                 $portListening | Should -Be $true
             }
