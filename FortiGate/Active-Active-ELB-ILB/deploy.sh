d2d187d436dff2173310e97862b8648529800e78
Joeri Van Hoof <jvanhoof@fortinet.com>
2020-05-26 17:37:44 +0200

    Update A/A deploy script with what if and output public IP Deploy Single VM with Basic IP

--- FortiGate/Active-Active-ELB-ILB/deploy.sh
+++ FortiGate/Active-Active-ELB-ILB/deploy.sh
@@ -83,19 +83,19 @@ az group create --location "$location" --name "$rg"
 
 # Validate template
 echo "--> Validation deployment in $rg resource group ..."
-#az deployment group validate --resource-group "$rg" \
-#                           --template-file azuredeploy.json \
-#                           --parameters adminUsername="$username" adminPassword=$passwd FortiGateNamePrefix=$prefix
-#result=$?
-#if [ $result != 0 ];
-#then
-#    echo "--> Validation failed ..."
-#    exit $rc;
-#fi
+az deployment group validate --resource-group "$rg" \
+                           --template-file azuredeploy.json \
+                           --parameters adminUsername="$username" adminPassword=$passwd FortiGateNamePrefix=$prefix
+result=$?
+if [ $result != 0 ];
+then
+    echo "--> Validation failed ..."
+    exit $rc;
+fi
 
 # Deploy resources
 echo "--> Deployment of $rg resources ..."
-az deployment group create --resource-group "$rg" \
+az deployment group create --confirm-with-what-if --resource-group "$rg" \
                            --template-file azuredeploy.json \
                            --parameters adminUsername="$username" adminPassword=$passwd FortiGateNamePrefix=$prefix
 result=$?
@@ -110,24 +110,30 @@ echo "
 # FortiGate Azure deployment using ARM Template
 # Active/Active loadbalanced pair of standalone FortiGates for resilience and scale
 #
-# The FortiGate systems is reachable via the public IP addresses of the load balancers
-# on HTTPS/443 and SSH/22.
+# You can access both management GUIs and SSH using the public IP address of the load balancer using HTTPS on
+# port 40030, 40031 and for SSH on port 50030 and 50031. The FortiGate VMs are also acessible using their
+# private IPs on the internal subnet using HTTPS on port 443 and SSH on port 22.
 #
 ##############################################################################################################
 
 Deployment information:
 
-Username: 
+Username:
 
 FortiGate IP addesses
 "
 query="[?virtualMachine.name.starts_with(@, '$prefix')].{virtualMachine:virtualMachine.name, publicIP:virtualMachine.network.publicIpAddresses[0].ipAddress,privateIP:virtualMachine.network.privateIpAddresses[0]}"
 az vm list-ip-addresses --query "$query" --output tsv
 echo "
+ IP Public Azure Load Balancer:"
+publicIpIds=$(az network lb show -g "$rg" -n "$prefix-ExternalLoadBalancer" --query "frontendIpConfigurations[].publicIpAddress.id" --out tsv)
+while read publicIpId; do
+    az network public-ip show --ids "$publicIpId" --query "{ ipAddress: ipAddress, fqdn: dnsSettings.fqdn }" --out tsv
+done <<< "$publicIpIds"
+echo "
 
 ##############################################################################################################
 "
 fi
-
 
 exit 0
