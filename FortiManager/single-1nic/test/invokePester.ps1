90638aaaa3feebac761a23d2562cd24a62413370
Joeri Van Hoof <jvanhoof@fortinet.com>
2020-06-14 02:22:14 +0200

    Update ARM TTK tests

--- FortiManager/single-1nic/test/invokePester.ps1
+++ FortiManager/single-1nic/test/invokePester.ps1
@@ -4,26 +4,6 @@ param (
 
 $SourceDir = "$env:BUILD_SOURCESDIRECTORY\$templatename"
 $TempDir = $env:TEMP
-$modulePath = Join-Path $TempDir Pester-master\Pester.psm1
-
-if (-not(Test-Path $modulePath)) {
-
-    # Note: PSGet and chocolatey are not supported in hosted vsts build agent
-    $tempFile = Join-Path $TempDir pester.zip
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    Invoke-WebRequest https://github.com/pester/Pester/archive/master.zip -OutFile $tempFile
-
-    [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') | Out-Null
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($tempFile, $tempDir)
-
-    Remove-Item $tempFile
-}
-
-Import-Module $modulePath -DisableNameChecking
-
-$outputFile = Join-Path $SourceDir "TEST-pester.xml";
-
-Invoke-Pester -Path $SourceDir -PassThru -OutputFile $outputFile -OutputFormat NUnitXml -EnableExit
 
 $modulePath = Join-Path $TempDir arm-ttk-master\arm-ttk\arm-ttk.psd1
 
@@ -45,3 +25,24 @@ Import-Module $modulePath -DisableNameChecking
 $outputFile = Join-Path $SourceDir "TEST-pester.xml";
 
 $TestResults = Test-AzTemplate -TemplatePath $SourceDir
+
+$modulePath = Join-Path $TempDir Pester-master\Pester.psm1
+
+if (-not(Test-Path $modulePath)) {
+
+    # Note: PSGet and chocolatey are not supported in hosted vsts build agent
+    $tempFile = Join-Path $TempDir pester.zip
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    Invoke-WebRequest https://github.com/pester/Pester/archive/master.zip -OutFile $tempFile
+
+    [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') | Out-Null
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($tempFile, $tempDir)
+
+    Remove-Item $tempFile
+}
+
+Import-Module $modulePath -DisableNameChecking
+
+$outputFile = Join-Path $SourceDir "TEST-pester.xml";
+
+Invoke-Pester -Path $SourceDir -PassThru -OutputFile $outputFile -OutputFormat NUnitXml -EnableExit
