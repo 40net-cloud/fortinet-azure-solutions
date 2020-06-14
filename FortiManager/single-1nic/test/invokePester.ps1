435093723463fa68c8253bd352ae3cee59499ed8
Joeri Van Hoof <jvanhoof@fortinet.com>
2020-06-14 02:21:18 +0200

    Running ARM TTK

--- FortiManager/single-1nic/test/invokePester.ps1
+++ FortiManager/single-1nic/test/invokePester.ps1
@@ -24,3 +24,24 @@ Import-Module $modulePath -DisableNameChecking
 $outputFile = Join-Path $SourceDir "TEST-pester.xml";
 
 Invoke-Pester -Path $SourceDir -PassThru -OutputFile $outputFile -OutputFormat NUnitXml -EnableExit
+
+$modulePath = Join-Path $TempDir arm-ttk-master\arm-ttk\arm-ttk.psd1
+
+if (-not(Test-Path $modulePath)) {
+
+    # Note: PSGet and chocolatey are not supported in hosted vsts build agent
+    $tempFile = Join-Path $TempDir arm-ttk.zip
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    Invoke-WebRequest https://github.com/Azure/arm-ttk/archive/master.zip -OutFile $tempFile
+
+    [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') | Out-Null
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($tempFile, $tempDir)
+
+    Remove-Item $tempFile
+}
+
+Import-Module $modulePath -DisableNameChecking
+
+$outputFile = Join-Path $SourceDir "TEST-pester.xml";
+
+$TestResults = Test-AzTemplate -TemplatePath $SourceDir
