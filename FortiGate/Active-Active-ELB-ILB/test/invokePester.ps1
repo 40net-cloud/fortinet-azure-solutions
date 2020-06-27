53d6d710b1a3e0d6689a22ea4a58111cc2aff904
Joeri Van Hoof <jvanhoof@fortinet.com>
2020-06-27 09:27:28 +0200

    Update

--- FortiGate/Active-Active-ELB-ILB/test/invokePester.ps1
+++ FortiGate/Active-Active-ELB-ILB/test/invokePester.ps1
@@ -38,11 +38,30 @@ if (-not(Test-Path $modulePath)) {
 
 Import-Module $modulePath -DisableNameChecking
 
+$modulePath = Join-Path $TempDir Export-NUnitXml.psm1
+
+if (-not(Test-Path $modulePath)) {
+
+    # Note: PSGet and chocolatey are not supported in hosted vsts build agent
+    $tempFile = Join-Path $TempDir pester.zip
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    Invoke-WebRequest https://raw.githubusercontent.com/sam-cogan/arm-ttk-extension/master/task/Export-NUnitXml.psm1 -OutFile $tempFile
+
+    [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') | Out-Null
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($tempFile, $tempDir)
+
+    Remove-Item $tempFile
+}
+
+Import-Module $modulePath -DisableNameChecking
+
 $outputFile = Join-Path $SourceDir "TEST-armttk.xml";
 
 "Running ARM TTK"
-Test-AzTemplate -TemplatePath $SourceDir -Pester
+$results = @(Test-AzTemplate -TemplatePath $SourceDir -Pester)
 
-$outputFile = Join-Path $SourceDir "TEST-pester.xml";
+Export-NUnitXml -TestResults $results -Path $resultlocation
+
+$outputFile = Join-Path $SourceDir "TEST-custom.xml";
 
 Invoke-Pester -Path $SourceDir -PassThru -OutputFile $outputFile -OutputFormat NUnitXml -EnableExit
