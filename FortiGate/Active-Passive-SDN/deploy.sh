0885cbc3f1b63ca20133a608d1a9732cdf78835b
Joeri Van Hoof <jvanhoof@fortinet.com>
2020-08-22 07:30:43 +0200

    Update Active/Passive SDN AZ template

--- FortiGate/Active-Passive-SDN/deploy.sh
+++ FortiGate/Active-Passive-SDN/deploy.sh
@@ -103,7 +103,7 @@ fi
 echo "--> Deployment of $rg resources ..."
 az deployment group create --resource-group "$rg" \
                            --template-file azuredeploy.json \
-                           --parameters adminUsername="$username" adminPassword="$PASSWORD" fortigateNamePrefix=$prefix
+                           --parameters adminUsername="$USERNAME" adminPassword="$PASSWORD" fortigateNamePrefix=$prefix
 result=$
 if [[ $result != 0 ]];
 then
