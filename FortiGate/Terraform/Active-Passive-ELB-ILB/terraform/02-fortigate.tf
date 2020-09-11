6fa25422b00120f3236d5953a22209f5b565ee6a
Joeri Van Hoof <jvanhoof@fortinet.com>
2020-09-11 09:11:46 +0200

    Update naming convention terraform

--- FortiGate/Terraform/Active-Passive-ELB-ILB/terraform/02-fortigate.tf
+++ FortiGate/Terraform/Active-Passive-ELB-ILB/terraform/02-fortigate.tf
@@ -144,7 +144,7 @@ resource "azurerm_lb_rule" "lb_haports_rule" {
 }
 
 resource "azurerm_network_interface" "fgtaifcext" {
-  name                          = "${var.PREFIX}-VM-FGT-A-IFC-EXT"
+  name                          = "${var.PREFIX}-A-VM-FGT-IFC-EXT"
   location                      = azurerm_resource_group.resourcegroup.location
   resource_group_name           = azurerm_resource_group.resourcegroup.name
   enable_ip_forwarding          = true
@@ -214,7 +214,7 @@ resource "azurerm_network_interface_security_group_association" "fgtaifchasyncns
 }
 
 resource "azurerm_public_ip" "fgtamgmtpip" {
-  name                = "${var.PREFIX}-FGT-A-MGMT-PIP"
+  name                = "${var.PREFIX}-A-FGT-MGMT-PIP"
   location            = var.LOCATION
   resource_group_name = azurerm_resource_group.resourcegroup.name
   allocation_method   = "Static"
@@ -328,7 +328,7 @@ data "template_file" "fgt_a_custom_data" {
 }
 
 resource "azurerm_network_interface" "fgtbifcext" {
-  name                          = "${var.PREFIX}-VM-FGT-B-IFC-EXT"
+  name                          = "${var.PREFIX}-B-VM-FGT-IFC-EXT"
   location                      = azurerm_resource_group.resourcegroup.location
   resource_group_name           = azurerm_resource_group.resourcegroup.name
   enable_ip_forwarding          = true
@@ -400,7 +400,7 @@ resource "azurerm_network_interface_security_group_association" "fgtbifchasyncns
 }
 
 resource "azurerm_public_ip" "fgtbmgmtpip" {
-  name                = "${var.PREFIX}-FGT-B-MGMT-PIP"
+  name                = "${var.PREFIX}-B-FGT-MGMT-PIP"
   location            = var.LOCATION
   resource_group_name = azurerm_resource_group.resourcegroup.name
   allocation_method   = "Static"
