3ea69379e4e566638436c7684fc56052a829bdbc
Joeri Van Hoof <jvanhoof@fortinet.com>
2020-10-08 07:28:21 +0200

    Update ui definition ARM TTK validation

--- FortiGate/AzureVirtualWAN/routing/deploy.sh
+++ FortiGate/AzureVirtualWAN/routing/deploy.sh
@@ -142,112 +142,148 @@ subnetSpoke2Prefix="172.16.162.0/28"
 vhubRouteTable1Name="RT_Shared"
 vhubRouteTable2Name="RT_V2B"
 
-
 # Create resource group
-#echo ""
-#echo "--> Creating $rg resource group ..."
-#az group create --location "$location" --name "$rg"
+echo ""
+echo "--> Creating $rg resource group ..."
+if [ $(az group exists --name $rg) = false ]; then
+    az group create --location "$location" --name "$rg"
+fi
 
 # Create Virtual Network Spoke1
-#echo ""
-#echo "--> Creating virtual network $vnetSpoke1Name [$vnetSpoke1Prefix]..."
-#az network vnet create \
-#    --resource-group "$rg" \
-#    --name $vnetSpoke1Name \
-#    --address-prefix "$vnetSpoke1Prefix" \
-#    --subnet-name "$subnetSpoke1Name" \
-#    --subnet-prefix "$subnetSpoke1Prefix"
+echo ""
+echo "--> Creating virtual network $vnetSpoke1Name [$vnetSpoke1Prefix]..."
+az network vnet show --name "$vnetSpoke1Name" --resource-group "$rg"
+if [ $? != 0 ];
+then
+    az network vnet create \
+        --resource-group "$rg" \
+        --name "$vnetSpoke1Name" \
+        --address-prefix "$vnetSpoke1Prefix" \
+        --subnet-name "$subnetSpoke1Name" \
+        --subnet-prefix "$subnetSpoke1Prefix"
+fi
 
 # Create Virtual Network Spoke2
-#echo ""
-#echo "--> Creating virtual network $vnetSpoke2Name [$vnetSpoke2Prefix]..."
-#az network vnet create \
-#    --resource-group "$rg" \
-#    --name $vnetSpoke2Name \
-#    --address-prefix "$vnetSpoke2Prefix" \
-#    --subnet-name "$subnetSpoke2Name" \
-#    --subnet-prefix "$subnetSpoke2Prefix"
+echo ""
+echo "--> Creating virtual network $vnetSpoke2Name [$vnetSpoke2Prefix]..."
+az network vnet show --name "$vnetSpoke2Name" --resource-group "$rg"
+if [ $? != 0 ];
+then
+    az network vnet create \
+        --resource-group "$rg" \
+        --name "$vnetSpoke2Name" \
+        --address-prefix "$vnetSpoke2Prefix" \
+        --subnet-name "$subnetSpoke2Name" \
+        --subnet-prefix "$subnetSpoke2Prefix"
+fi
 
 # Deploy FortiGate next-generation firewall
-#echo "--> Deployment of FortiGate NGFW ..."
-#az deployment group create --resource-group "$rg" \
-#                           --template-uri "https://raw.githubusercontent.com/40net-cloud/fortinet-azure-solutions/main/FortiGate/A-Single-VM/azuredeploy.json" \
-#                           --parameters adminUsername="$USERNAME" adminPassword="$PASSWORD" fortigateNamePrefix="$prefix"
-#result=$?
-#if [[ $result != 0 ]];
-#then
-#    echo "--> Deployment failed ..."
-#    exit $result;
-#fi
+echo "--> Deployment of FortiGate NGFW ..."
+az deployment group create --resource-group "$rg" \
+                           --template-uri "https://raw.githubusercontent.com/40net-cloud/fortinet-azure-solutions/main/FortiGate/A-Single-VM/azuredeploy.json" \
+                           --parameters adminUsername="$USERNAME" adminPassword="$PASSWORD" fortigateNamePrefix="$prefix"
+result=$?
+if [[ $result != 0 ]];
+then
+    echo "--> Deployment failed ..."
+    exit $result;
+fi
 
 # Create Virtual WAN
-#echo ""
-#echo "--> Creating Azure Virtual WAN $vwanName ..."
-#az network vwan create --name "$vwanName" --resource-group "$rg"
+echo ""
+echo "--> Creating Azure Virtual WAN $vwanName ..."
+az network vwan show --name "$vwanName" --resource-group "$rg"
+if [ $? != 0 ];
+then
+    az network vwan create --name "$vwanName" --resource-group "$rg"
+fi
 
 # Create Virtual HUB
-#echo ""
-#echo "--> Creating Azure Virtual HUB $vhubName ..."
-#az network vhub create \
-#    --name "$vhubName" \
-#    --resource-group "$rg" \
-#    --address-prefix "$vhubAddressPrefix" \
-#    --location "$location" \
-#    --vwan "$vwanName" \
-#    --sku "Standard"
-
+echo ""
+echo "--> Creating Azure Virtual HUB $vhubName ..."
+az network vhub show --name "$vwanName" --resource-group "$rg"
+if [ $? != 0 ];
+then
+    az network vhub create \
+        --name "$vhubName" \
+        --resource-group "$rg" \
+        --address-prefix "$vhubAddressPrefix" \
+        --location "$location" \
+        --vwan "$vwanName" \
+        --sku "Standard"
+fi
 vhubRouteTableDefaultId=$(az network vhub route-table show --name defaultRouteTable --resource-group $rg --vhub-name $vhubName --query "id")
 
 # Create Virtual HUB Route Table RT_Shared
-#echo ""
-#echo "--> Creating Azure Virtual HUB route table $vhubRouteTable1Name ..."
-#az network vhub route-table create \
-#    --name "$vhubRouteTable1Name" \
-#    --resource-group "$rg" \
-#    --vhub-name "$vhubName"
+echo ""
+echo "--> Creating Azure Virtual HUB route table $vhubRouteTable1Name ..."
+az network vhub route-table show --name "$vhubRouteTable1Name" --resource-group "$rg" --vhub-name "$vhubName"
+if [ $? != 0 ];
+then
+    az network vhub route-table create \
+        --name "$vhubRouteTable1Name" \
+        --resource-group "$rg" \
+        --vhub-name "$vhubName"
+fi
 vhubRouteTable1Id=$(az network vhub route-table show --name $vhubRouteTable1Name --resource-group $rg --vhub-name $vhubName --output tsv --query "id")
 
 # Create Virtual HUB Route Table RT_V2B
-#echo ""
-#echo "--> Creating Azure Virtual HUB route table $vhubRouteTable2Name ..."
-##az network vhub route-table create \
-#    --name "$vhubRouteTable2Name" \
-#    --resource-group "$rg" \
-#    --vhub-name "$vhubName"
+echo ""
+echo "--> Creating Azure Virtual HUB route table $vhubRouteTable2Name ..."
+az network vhub route-table --name "$vhubRouteTable2Name" --resource-group "$rg" --vhub-name "$vhubName"
+if [ $? != 0 ];
+then
+    az network vhub route-table create \
+        --name "$vhubRouteTable2Name" \
+        --resource-group "$rg" \
+        --vhub-name "$vhubName"
+fi
 vhubRouteTable2Id=$(az network vhub route-table show --name $vhubRouteTable2Name --resource-group $rg --vhub-name $vhubName --output tsv --query "id")
 
 # Create Virtual HUB peering to spoke1
 echo ""
 echo "--> Creating Azure Virtual HUB peering to $vnetSpoke1Name ..."
-az network vhub connection create \
-    --name "$vhubNameTo$vnetSpoke1Name" \
-    --resource-group "$rg" \
-    --vhub-name "$vhubName" \
-    --associated-route-table "$vhubRouteTable2Id" \
-    --propagated-route-tables "$vhubRouteTable1Id" "$vhubRouteTable2Id" \
-    --remote-vnet "$vnetSpoke1Name"
+az network vhub connection --name "$vhubNameTo$vnetSpoke1Name" --resource-group "$rg" --vhub-name "$vhubName"
+if [ $? != 0 ];
+then
+    az network vhub connection create \
+        --name "$vhubNameTo$vnetSpoke1Name" \
+        --resource-group "$rg" \
+        --vhub-name "$vhubName" \
+        --associated-route-table "$vhubRouteTable2Id" \
+        --propagated-route-tables "$vhubRouteTable1Id" "$vhubRouteTable2Id" \
+        --remote-vnet "$vnetSpoke1Name"
+fi
 
 # Create Virtual HUB peering to spoke2
 echo ""
 echo "--> Creating Azure Virtual HUB peering to $vnetSpoke2Name ..."
-az network vhub connection create \
-    --name "$vhubNameTo$vnetSpoke2Name" \
-    --resource-group "$rg" \
-    --vhub-name "$vhubName" \
-    --associated-route-table "$vhubRouteTable2Id" \
-    --propagated-route-tables "$vhubRouteTable1Id" "$vhubRouteTable2Id" \
-    --remote-vnet "$vnetSpoke2Name"
+az network vhub connection --name "$vhubNameTo$vnetSpoke2Name" --resource-group "$rg" --vhub-name "$vhubName"
+if [ $? != 0 ];
+then
+    az network vhub connection create \
+        --name "$vhubNameTo$vnetSpoke2Name" \
+        --resource-group "$rg" \
+        --vhub-name "$vhubName" \
+        --associated-route-table "$vhubRouteTable2Id" \
+        --propagated-route-tables "$vhubRouteTable1Id" "$vhubRouteTable2Id" \
+        --remote-vnet "$vnetSpoke2Name"
+fi
 
 # Create Virtual HUB peering to FGT
 echo ""
-echo "--> Creating Azure Virtual HUB peering to $vnetSpoke2Name ..."
-az network vhub connection create \
-    --name "$vhubNameTo$vnetSpoke2Name" \
-    --resource-group "$rg" \
-    --vhub-name "$vhubName" \
-    --associated-route-table "$vhubRouteTable2Id" \
-    --propagated-route-tables "$vhubRouteTable1Id" "$vhubRouteTable2Id" \
-    --remote-vnet "$vnetSpoke2Name"
+echo "--> Creating Azure Virtual HUB peering to $vnetSpokeFGTName ..."
+az network vhub connection show --name "$vhubNameTo$vnetSpokeFGTName" --resource-group "$rg" --vhub-name "$vhubName"
+if [ $? != 0 ];
+then
+    az network vhub connection create \
+        --name "$vhubNameTo$vnetSpokeFGTName" \
+        --resource-group "$rg" \
+        --vhub-name "$vhubName" \
+        --associated-route-table "$vhubRouteTable1Id" \
+        --propagated-route-tables "$vhubRouteTable1Id" \
+        --remote-vnet "$vnetSpokeFGTName"
+fi
 
 echo "
 ##############################################################################################################
