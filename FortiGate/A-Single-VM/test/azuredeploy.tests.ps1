63a3a6dde1c65a8a021430209cad923726840784
Joeri Van Hoof <jvanhoof@fortinet.com>
2021-03-15 09:16:47 +0100

    Update tests

--- FortiGate/A-Single-VM/test/azuredeploy.tests.ps1
+++ FortiGate/A-Single-VM/test/azuredeploy.tests.ps1
@@ -62,7 +62,6 @@ Describe 'FGT Single VM' {
         It 'Creates the expected Azure resources' {
             $expectedResources = 'Microsoft.Resources/deployments',
                                  'Microsoft.Network/routeTables',
-                                 'Microsoft.Network/routeTables',
                                  'Microsoft.Network/virtualNetworks',
                                  'Microsoft.Network/networkSecurityGroups',
                                  'Microsoft.Network/publicIPAddresses',
