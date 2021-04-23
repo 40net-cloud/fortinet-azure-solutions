c973ad4922ff38c88b25874d6ae925fe46557fcb
Joeri Van Hoof <jvanhoof@fortinet.com>
2021-04-24 00:33:03 +0200

    Test 6

--- FortiGate/A-Single-VM/test/azuredeploy.tests.ps1
+++ FortiGate/A-Single-VM/test/azuredeploy.tests.ps1
@@ -9,25 +9,9 @@
 #>
 
 param (
-    [string]$sshkey
+    [string]$sshkey,
     [string]$sshkeypub
 )
-
-Function random-password ($length = 15) {
-    $punc = 46..46
-    $digits = 48..57
-    $letters = 65..90 + 97..122
-
-    # Thanks to
-    # https://blogs.technet.com/b/heyscriptingguy/archive/2012/01/07/use-pow
-    $password = get-random -count $length `
-        -input ($punc + $digits + $letters) |
-        % -begin { $aa = $null } `
-        -process {$aa += [char]$_} `
-        -end {$aa}
-
-    return $password
-}
 
 $templateName = "A-Single-VM"
 $sourcePath = "$env:BUILD_SOURCESDIRECTORY\FortiGate\$templateName"
