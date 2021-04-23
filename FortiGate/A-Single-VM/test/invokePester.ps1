2d17dd91e92178a626724323617a6fe4683bec19
Joeri Van Hoof <jvanhoof@fortinet.com>
2021-04-23 17:21:07 +0200

    test

--- FortiGate/A-Single-VM/test/invokePester.ps1
+++ FortiGate/A-Single-VM/test/invokePester.ps1
@@ -49,6 +49,8 @@ if (-not(Test-Path $modulePath)) {
 
 Import-Module $modulePath -DisableNameChecking
 
+Get-Content $(sshkeypub.secureFilePath)
+
 $outputFile = Join-Path $SourceDir "TEST-armttk.xml";
 
 "Running ARM TTK"
