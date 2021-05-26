ffb7ba8fe4a3f4451ac7325380f1bdca38a7a67b
Joeri Van Hoof <jvanhoof@fortinet.com>
2021-05-26 20:02:40 +0200

    Removed resource group from ilbbackend

--- FortiGate/Terraform/VNET-Peering/terraform/03-fortigate.tf
+++ FortiGate/Terraform/VNET-Peering/terraform/03-fortigate.tf
@@ -151,7 +151,6 @@ resource "azurerm_lb" "ilb" {
 }
 
 resource "azurerm_lb_backend_address_pool" "ilbbackend" {
-  resource_group_name = azurerm_resource_group.resourcegroup.name
   loadbalancer_id     = azurerm_lb.ilb.id
   name                = "BackEndPool"
 }
