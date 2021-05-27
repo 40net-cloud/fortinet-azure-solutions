2d3406884bcad8e0d593fd1893c93abebe67b811
Joeri Van Hoof <jvanhoof@fortinet.com>
2021-05-27 15:29:43 +0200

    Update results summary for Terraform

--- FortiGate/Terraform/Active-Active-ELB-ILB/deploy.sh
+++ FortiGate/Terraform/Active-Active-ELB-ILB/deploy.sh
@@ -80,7 +80,7 @@ rg_cgf="$prefix-RG"
 if [ -z "$DEPLOY_USERNAME" ]
 then
     # Input username
-    echo -n "Enter username: "
+    echo -n "Enter username (default: azureuser): "
     stty_orig=`stty -g` # save original terminal setting.
     read USERNAME         # read the prefix
     stty $stty_orig     # restore terminal setting.
@@ -146,25 +146,4 @@ echo ""
 terraform output deployment_summary > "../output/$SUMMARY"
 
 cd ../
-echo "
-##############################################################################################################
-#
-# FortiGate Active/Active Load Balanced pair of standalone FortiGate VMs for resilience and scale
-# Terraform deployment template for Microsoft Azure
-#
-# The FortiGate VMs are reachable via the public IP address of the load balancer.
-# Management GUI HTTPS on port 40030, 40031 and for SSH on port 50030 and 50031.
-#
-# BEWARE: The state files contain sensitive data like passwords and others. After the demo clean up your
-#         clouddrive directory.
-#
-##############################################################################################################
-
-Deployment information:
-
-Username: $USERNAME
-"
 cat "output/$SUMMARY"
-echo "
-
-##############################################################################################################"
