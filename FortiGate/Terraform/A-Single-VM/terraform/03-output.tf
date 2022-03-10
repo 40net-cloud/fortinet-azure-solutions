7bbfccd5fb353b7189bef1f376ea8bf925eeb2b6
Joeri <jvanhoof@fortinet.com>
2022-03-10 13:53:02 +0100

    [FGT] Fixed Terraform formatting

--- FortiGate/Terraform/A-Single-VM/terraform/03-output.tf
+++ FortiGate/Terraform/A-Single-VM/terraform/03-output.tf
@@ -13,11 +13,11 @@ data "template_file" "summary" {
   template = file("${path.module}/summary.tpl")
 
   vars = {
-    username                     = var.USERNAME
-    location                     = var.LOCATION
-    fgt_ipaddress                = data.azurerm_public_ip.fgtpip.ip_address
-    fgt_private_ip_address_ext   = azurerm_network_interface.fgtifcext.private_ip_address
-    fgt_private_ip_address_int   = azurerm_network_interface.fgtifcint.private_ip_address
+    username                   = var.USERNAME
+    location                   = var.LOCATION
+    fgt_ipaddress              = data.azurerm_public_ip.fgtpip.ip_address
+    fgt_private_ip_address_ext = azurerm_network_interface.fgtifcext.private_ip_address
+    fgt_private_ip_address_int = azurerm_network_interface.fgtifcint.private_ip_address
   }
 }
 
