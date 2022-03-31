d85473fc3cf640f1d459bd069437886c3f8fccd9
Joeri <jvanhoof@fortinet.com>
2022-03-31 19:41:11 +0200

    [FTS] Updated tests port 443 [FTS] location in parameters file should be empty

--- FortiTester/Standalone/test/azuredeploy.tests.ps1
+++ FortiTester/Standalone/test/azuredeploy.tests.ps1
@@ -37,7 +37,7 @@ BeforeAll {
                     'fortiTesterNamePrefix'=$testsPrefix
                     'publicIPName'=$publicIPName
                 }
-    $ports = @(8443, 22)
+    $ports = @(443, 22)
 }
 
 Describe 'FTS Standalone' {
