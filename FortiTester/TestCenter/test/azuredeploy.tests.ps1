5f09d57015c4b94374e9806cb7c411971fa03681
Joeri <jvanhoof@fortinet.com>
2022-03-31 13:32:17 +0200

    [FTS] Add tests for FortiTester

---
+++ FortiTester/TestCenter/test/azuredeploy.tests.ps1
@@ -0,0 +1,150 @@
+#Requires -Modules Pester
+<#
+.SYNOPSIS
+    Tests a specific ARM template
+.EXAMPLE
+    Invoke-Pester
+.NOTES
+    This file has been created as an example of using Pester to evaluate ARM templates
+#>
+
+param (
+    [string]$sshkey,
+    [string]$sshkeypub
+)
+$VerbosePreference = "Continue"
+
+BeforeAll {
+    $templateName = "TestCenter"
+    $sourcePath = "$env:GITHUB_WORKSPACE\FortiTester\$templateName"
+    $scriptPath = "$env:GITHUB_WORKSPACE\FortiTester\$templateName\test"
+    $templateFileName = "mainTemplate.json"
+    $templateFileLocation = "$sourcePath\$templateFileName"
+    $templateParameterFileName = "mainTemplate.parameters.json"
+    $templateParameterFileLocation = "$sourcePath\$templateParameterFileName"
+
+    # Basic Variables
+    $testsRandom = Get-Random 10001
+    $testsPrefix = "FORTIQA"
+    $testsResourceGroupName = "FORTIQA-$testsRandom-$templateName"
+    $testsAdminUsername = "azureuser"
+    $testsResourceGroupLocation = "westeurope"
+
+    # ARM Template Variables
+    $publicIPName = "$testsPrefix-FTS-PIP"
+    $params = @{ 'adminUsername'=$testsAdminUsername
+                    'adminPassword'=$testsResourceGroupName
+                    'fortiTesterNamePrefix'=$testsPrefix
+                    'publicIPName'=$publicIPName
+                }
+    $ports = @(8443, 22)
+}
+
+Describe 'FTS TestCenter' {
+    Context 'Validation' {
+        It 'Has a JSON template' {
+            $templateFileLocation | Should -Exist
+        }
+
+        It 'Has a parameters file' {
+            $templateParameterFileLocation | Should -Exist
+        }
+
+        It 'Converts from JSON and has the expected properties' {
+            $expectedProperties = '$schema',
+            'contentVersion',
+            'parameters',
+            'resources',
+            'variables'
+            $templateProperties = (get-content $templateFileLocation | ConvertFrom-Json -ErrorAction SilentlyContinue) | Get-Member -MemberType NoteProperty | % Name
+            $templateProperties | Should -Be $expectedProperties
+        }
+
+        It 'Creates the expected Azure resources' {
+            $expectedResources = 'Microsoft.Resources/deployments',
+                                    'Microsoft.Storage/storageAccounts',
+                                    'Microsoft.Network/virtualNetworks',
+                                    'Microsoft.Network/networkSecurityGroups',
+                                    'Microsoft.Network/publicIPAddresses',
+                                    'Microsoft.Network/networkInterfaces',
+                                    'Microsoft.Network/networkInterfaces',
+                                    'Microsoft.Compute/virtualMachines'
+            $templateResources = (get-content $templateFileLocation | ConvertFrom-Json -ErrorAction SilentlyContinue).Resources.type
+            $templateResources | Should -Be $expectedResources
+        }
+
+        It 'Contains the expected parameters' {
+            $expectedTemplateParameters = 'acceleratedNetworking',
+                                            'adminPassword',
+                                            'adminUsername',
+                                            'fortinetTags',
+                                            'fortiTesterAdditionalCustomData',
+                                            'fortiTesterImageSKU',
+                                            'fortiTesterImageVersion',
+                                            'fortiTesterNamePrefix',
+                                            'instanceType',
+                                            'location',
+                                            'publicIPName',
+                                            'publicIPNewOrExistingOrNone',
+                                            'publicIPResourceGroup',
+                                            'publicIPSKU',
+                                            'publicIPType',
+                                            'serialConsole',
+                                            'subnet1Name',
+                                            'subnet1Prefix',
+                                            'subnet1StartAddress',
+                                            'subnet2Name',
+                                            'subnet2Prefix',
+                                            'subnet2StartAddress',
+                                            'vnetAddressPrefix',
+                                            'vnetName',
+                                            'vnetNewOrExisting',
+                                            'vnetResourceGroup'
+            $templateParameters = (get-content $templateFileLocation | ConvertFrom-Json -ErrorAction SilentlyContinue).Parameters | Get-Member -MemberType NoteProperty | % Name | Sort-Object
+            $templateParameters | Should -Be $expectedTemplateParameters
+        }
+
+    }
+
+    Context 'Deployment' {
+
+        It "Test Deployment" {
+            New-AzResourceGroup -Name $testsResourceGroupName -Location "$testsResourceGroupLocation"
+            (Test-AzResourceGroupDeployment -ResourceGroupName "$testsResourceGroupName" -TemplateFile "$templateFileLocation" -TemplateParameterObject $params).Count | Should -Not -BeGreaterThan 0
+        }
+        It "Deployment" {
+            Write-Host ( "Deployment name: $testsResourceGroupName" )
+
+            $resultDeployment = New-AzResourceGroupDeployment -ResourceGroupName "$testsResourceGroupName" -TemplateFile "$templateFileLocation" -TemplateParameterObject $params
+            Write-Host ($resultDeployment | Format-Table | Out-String)
+            Write-Host ("Deployment state: " + $resultDeployment.ProvisioningState | Out-String)
+            $resultDeployment.ProvisioningState | Should -Be "Succeeded"
+        }
+        It "Search deployment" {
+            $result = Get-AzVM | Where-Object { $_.Name -like "$testsPrefix*" }
+            Write-Host ($result | Format-Table | Out-String)
+            $result | Should -Not -Be $null
+        }
+    }
+
+    Context 'Deployment test' {
+
+        BeforeAll {
+            $FTS = (Get-AzPublicIpAddress -Name $publicIPName -ResourceGroupName $testsResourceGroupName).IpAddress
+            Write-Host ("FortiTester public IP: " + $FTS)
+        }
+        It "FTS: Ports listening" {
+            ForEach( $port in $ports ) {
+                Write-Host ("Check port: $port" )
+                $portListening = (Test-Connection -TargetName $FTS -TCPPort $port -TimeoutSeconds 100)
+                $portListening | Should -Be $true
+            }
+        }
+    }
+
+    Context 'Cleanup' {
+        It "Cleanup of deployment" {
+            Remove-AzResourceGroup -Name $testsResourceGroupName -Force
+        }
+    }
+}
