b18689368073ce6446e5fe51ea61be32048a7aca
jvhoof <jvanhoof@fortinet.com>
2023-01-30 14:12:59 +0100

    FGT IPSEC tunnel: allow port 80 smokeping

--- FortiGate/Playground/IPSEC-tunnels/terraform-aws/01-network.tf
+++ FortiGate/Playground/IPSEC-tunnels/terraform-aws/01-network.tf
@@ -151,6 +151,13 @@ resource "aws_security_group" "public_allow" {
   }
 
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "6"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "6"
