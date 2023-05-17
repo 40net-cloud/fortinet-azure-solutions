4bf6585436d5f51175dcfde7e7ec94b3f011e1a1
jvhoof <jvanhoof@fortinet.com>
2023-05-17 16:52:41 +0200

    FMG HA: Terraform

---
+++ FortiManager/Terraform/ha/terraform/03-output.tf
@@ -0,0 +1,29 @@
+##############################################################################################################
+#
+# FortiManager - a standalone FortiManager VM
+# Terraform deployment template for Microsoft Azure
+#
+##############################################################################################################
+#
+# Output summary of deployment
+#
+##############################################################################################################
+
+output "deployment_summary" {
+  value = templatefile("${path.module}/summary.tpl", {
+    location               = var.LOCATION
+    fmg_username           = var.USERNAME
+    fmg_a_public_ip_address  = data.azurerm_public_ip.fmgapip.ip_address
+    fmg_a_private_ip_address = azurerm_network_interface.fmgaifc.private_ip_address
+    fmg_b_public_ip_address  = data.azurerm_public_ip.fmgbpip.ip_address
+    fmg_b_private_ip_address = azurerm_network_interface.fmgbifc.private_ip_address
+  })
+}
+
+output "vnet_id" {
+  value = azurerm_virtual_network.vnet.id
+}
+
+output "subnet_id" {
+  value = azurerm_subnet.subnet1.id
+}
