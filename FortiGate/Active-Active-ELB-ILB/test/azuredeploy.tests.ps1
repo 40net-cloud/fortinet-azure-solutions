70f697a76f35bd385dda60a4aee170ea7bede8d0
Joeri Van Hoof <jvanhoof@fortinet.com>
2024-05-22 11:42:39 +0200

    FGT A/A: debug

--- FortiGate/Active-Active-ELB-ILB/test/azuredeploy.tests.ps1
+++ FortiGate/Active-Active-ELB-ILB/test/azuredeploy.tests.ps1
@@ -9,167 +9,169 @@
 #>
 
 param (
-    [string]$sshkey,
-    [string]$sshkeypub
+  [string]$sshkey,
+  [string]$sshkeypub
 )
 
 BeforeAll {
-    $templateName = "Active-Active-ELB-ILB"
-    $sourcePath = "$env:GITHUB_WORKSPACE\FortiGate\$templateName"
-    $scriptPath = "$env:GITHUB_WORKSPACE\FortiGate\$templateName\test"
-    $templateFileName = "azuredeploy.json"
-    $templateFileLocation = "$sourcePath\$templateFileName"
-    $templateParameterFileName = "azuredeploy.parameters.json"
-    $templateParameterFileLocation = "$sourcePath\$templateParameterFileName"
+  $templateName = "Active-Active-ELB-ILB"
+  $sourcePath = "$env:GITHUB_WORKSPACE\FortiGate\$templateName"
+  $scriptPath = "$env:GITHUB_WORKSPACE\FortiGate\$templateName\test"
+  $templateFileName = "azuredeploy.json"
+  $templateFileLocation = "$sourcePath\$templateFileName"
+  $templateParameterFileName = "azuredeploy.parameters.json"
+  $templateParameterFileLocation = "$sourcePath\$templateParameterFileName"
 
-    # Basic Variables
-    $testsRandom = Get-Random 10001
-    $testsPrefix = "FORTIQA"
-    $testsResourceGroupName = "FORTIQA-$testsRandom-$templateName"
-    $testsAdminUsername = "azureuser"
-    $testsResourceGroupLocation = "westeurope"
+  # Basic Variables
+  $testsRandom = Get-Random 10001
+  $testsPrefix = "FORTIQA"
+  $testsResourceGroupName = "FORTIQA-$testsRandom-$templateName"
+  $testsAdminUsername = "azureuser"
+  $testsResourceGroupLocation = "westeurope"
 
-    # ARM Template Variables
-    $config = "config system global `n set gui-theme mariner `n end `n config system admin `n edit devops `n set accprofile super_admin `n set ssh-public-key1 `""
-    $config += Get-Content $sshkeypub
-    $config += "`" `n set password $testsResourceGroupName `n next `n end"
-    $publicIPName = "$testsPrefix-externalloadbalancer-pip"
-    $fortiGateCount = 3
-    $params = @{ 'adminUsername'=$testsAdminUsername
-                 'adminPassword'=$testsResourceGroupName
-                 'fortiGateNamePrefix'=$testsPrefix
-                 'fortiGateAdditionalCustomData'=$config
-                 'fortiGateCount'=$fortiGateCount
-               }
-    $ports = @(40030, 50030, 40031, 50031)
+  # ARM Template Variables
+  $config = "config system global `n set gui-theme mariner `n end `n config system admin `n edit devops `n set accprofile super_admin `n set ssh-public-key1 `""
+  $config += Get-Content $sshkeypub
+  $config += "`" `n set password $testsResourceGroupName `n next `n end"
+  $publicIPName = "$testsPrefix-externalloadbalancer-pip"
+  $fortiGateCount = 3
+  $params = @{ 'adminUsername'      = $testsAdminUsername
+    'adminPassword'                 = $testsResourceGroupName
+    'fortiGateNamePrefix'           = $testsPrefix
+    'fortiGateAdditionalCustomData' = $config
+    'fortiGateCount'                = $fortiGateCount
+  }
+  $ports = @(40030, 50030, 40031, 50031)
 }
 
 Describe 'FGT A/A' {
-    Context 'Validation' {
-        It 'Has a JSON template' {
-            $templateFileLocation | Should -Exist
-        }
-
-        It 'Has a parameters file' {
-            $templateParameterFileLocation | Should -Exist
-        }
-
-        It 'Converts from JSON and has the expected properties' {
-            $expectedProperties = '$schema',
-            'contentVersion',
-            'functions',
-            'outputs',
-            'parameters',
-            'resources',
-            'variables'
-            $templateProperties = (get-content $templateFileLocation | ConvertFrom-Json -ErrorAction SilentlyContinue) | Get-Member -MemberType NoteProperty | % Name
-            $templateProperties | Should -Be $expectedProperties
-        }
-
-        It 'Creates the expected Azure resources' {
-            $expectedResources = 'Microsoft.Resources/deployments',
-                                 'Microsoft.Compute/availabilitySets',
-                                 'Microsoft.Network/virtualNetworks',
-                                 'Microsoft.Network/virtualNetworks/subnets',
-                                 'Microsoft.Network/natGateways',
-                                 'Microsoft.Network/routeTables',
-                                 'Microsoft.Network/networkSecurityGroups',
-                                 'Microsoft.Network/publicIPAddresses',
-                                 'Microsoft.Network/publicIPAddresses',
-                                 'Microsoft.Network/publicIPAddresses',
-                                 'Microsoft.Network/loadBalancers',
-                                 'Microsoft.Network/loadBalancers/inboundNatRules',
-                                 'Microsoft.Network/loadBalancers/inboundNatRules',
-                                 'Microsoft.Network/loadBalancers',
-                                 'Microsoft.Network/networkInterfaces',
-                                 'Microsoft.Network/networkInterfaces',
-                                 'Microsoft.Compute/virtualMachines'
-            $templateResources = (get-content $templateFileLocation | ConvertFrom-Json -ErrorAction SilentlyContinue).Resources.type
-            $templateResources | Should -Be $expectedResources
-        }
-
-        It 'Contains the expected parameters' {
-            $expectedTemplateParameters = 'acceleratedConnections',
-                                          'acceleratedConnectionsSku',
-                                          'acceleratedNetworking',
-                                          'adminPassword',
-                                          'adminUsername',
-                                          'availabilityOptions',
-                                          'customImageReference',
-                                          'externalLoadBalancer',
-                                          'fortiGateAdditionalCustomData',
-                                          'fortiGateConfigSync',
-                                          'fortiGateCount',
-                                          'fortiGateImageSKU',
-                                          'fortiGateImageVersion',
-                                          'fortiGateLicenseBYOL1',
-                                          'fortiGateLicenseBYOL2',
-                                          'fortiGateLicenseBYOL3',
-                                          'fortiGateLicenseBYOL4',
-                                          'fortiGateLicenseBYOL5',
-                                          'fortiGateLicenseBYOL6',
-                                          'fortiGateLicenseBYOL7',
-                                          'fortiGateLicenseBYOL8',
-                                          'fortiGateLicenseFortiFlex1',
-                                          'fortiGateLicenseFortiFlex2',
-                                          'fortiGateLicenseFortiFlex3',
-                                          'fortiGateLicenseFortiFlex4',
-                                          'fortiGateLicenseFortiFlex5',
-                                          'fortiGateLicenseFortiFlex6',
-                                          'fortiGateLicenseFortiFlex7',
-                                          'fortiGateLicenseFortiFlex8',
-                                          'fortiGateNamePrefix',
-                                          'fortiGateSessionSync',
-                                          'fortiManager',
-                                          'fortiManagerIP',
-                                          'fortiManagerSerial',
-                                          'fortinetTags',
-                                          'instanceType',
-                                          'location',
-                                          'outboundConnectivity',
-                                          'serialConsole',
-                                          'subnet1Name',
-                                          'subnet1Prefix',
-                                          'subnet1StartAddress',
-                                          'subnet2Name',
-                                          'subnet2Prefix',
-                                          'subnet2StartAddress',
-                                          'tagsByResource',
-                                          'vnetAddressPrefix',
-                                          'vnetName',
-                                          'vnetNewOrExisting',
-                                          'vnetResourceGroup'
-            $templateParameters = (get-content $templateFileLocation | ConvertFrom-Json -ErrorAction SilentlyContinue).Parameters | Get-Member -MemberType NoteProperty | % Name | sort
-            $templateParameters | Should -Be $expectedTemplateParameters
-        }
-
+  Context 'Validation' {
+    It 'Has a JSON template' {
+      $templateFileLocation | Should -Exist
     }
 
-    Context 'Deployment' {
+    It 'Has a parameters file' {
+      $templateParameterFileLocation | Should -Exist
+    }
 
-        It "Test deployment" {
-            New-AzResourceGroup -Name $testsResourceGroupName -Location "$testsResourceGroupLocation"
+    It 'Converts from JSON and has the expected properties' {
+      $expectedProperties = '$schema',
+      'contentVersion',
+      'functions',
+      'outputs',
+      'parameters',
+      'resources',
+      'variables'
+      $templateProperties = (get-content $templateFileLocation | ConvertFrom-Json -ErrorAction SilentlyContinue) | Get-Member -MemberType NoteProperty | % Name
+      $templateProperties | Should -Be $expectedProperties
+    }
+
+    It 'Creates the expected Azure resources' {
+      $expectedResources = 'Microsoft.Resources/deployments',
+      'Microsoft.Compute/availabilitySets',
+      'Microsoft.Network/virtualNetworks',
+      'Microsoft.Network/virtualNetworks/subnets',
+      'Microsoft.Network/natGateways',
+      'Microsoft.Network/routeTables',
+      'Microsoft.Network/networkSecurityGroups',
+      'Microsoft.Network/publicIPAddresses',
+      'Microsoft.Network/publicIPAddresses',
+      'Microsoft.Network/publicIPAddresses',
+      'Microsoft.Network/loadBalancers',
+      'Microsoft.Network/loadBalancers/inboundNatRules',
+      'Microsoft.Network/loadBalancers/inboundNatRules',
+      'Microsoft.Network/loadBalancers',
+      'Microsoft.Network/networkInterfaces',
+      'Microsoft.Network/networkInterfaces',
+      'Microsoft.Compute/virtualMachines'
+      $templateResources = (get-content $templateFileLocation | ConvertFrom-Json -ErrorAction SilentlyContinue).Resources.type
+      $templateResources | Should -Be $expectedResources
+    }
+
+    It 'Contains the expected parameters' {
+      $expectedTemplateParameters = 'acceleratedConnections',
+      'acceleratedConnectionsSku',
+      'acceleratedNetworking',
+      'adminPassword',
+      'adminUsername',
+      'availabilityOptions',
+      'customImageReference',
+      'externalLoadBalancer',
+      'fortiGateAdditionalCustomData',
+      'fortiGateConfigSync',
+      'fortiGateCount',
+      'fortiGateImageSKU',
+      'fortiGateImageVersion',
+      'fortiGateLicenseBYOL1',
+      'fortiGateLicenseBYOL2',
+      'fortiGateLicenseBYOL3',
+      'fortiGateLicenseBYOL4',
+      'fortiGateLicenseBYOL5',
+      'fortiGateLicenseBYOL6',
+      'fortiGateLicenseBYOL7',
+      'fortiGateLicenseBYOL8',
+      'fortiGateLicenseFortiFlex1',
+      'fortiGateLicenseFortiFlex2',
+      'fortiGateLicenseFortiFlex3',
+      'fortiGateLicenseFortiFlex4',
+      'fortiGateLicenseFortiFlex5',
+      'fortiGateLicenseFortiFlex6',
+      'fortiGateLicenseFortiFlex7',
+      'fortiGateLicenseFortiFlex8',
+      'fortiGateNamePrefix',
+      'fortiGateSessionSync',
+      'fortiManager',
+      'fortiManagerIP',
+      'fortiManagerSerial',
+      'fortinetTags',
+      'instanceType',
+      'location',
+      'outboundConnectivity',
+      'serialConsole',
+      'subnet1Name',
+      'subnet1Prefix',
+      'subnet1StartAddress',
+      'subnet2Name',
+      'subnet2Prefix',
+      'subnet2StartAddress',
+      'tagsByResource',
+      'vnetAddressPrefix',
+      'vnetName',
+      'vnetNewOrExisting',
+      'vnetResourceGroup'
+      $templateParameters = (get-content $templateFileLocation | ConvertFrom-Json -ErrorAction SilentlyContinue).Parameters | Get-Member -MemberType NoteProperty | % Name | sort
+      Write-Host ( "Expected: $expectedTemplateParameters" )
+      Write-Host ( "Received: $templateParameters" )
+      $templateParameters | Should -Be $expectedTemplateParameters
+    }
+
+  }
+
+  Context 'Deployment' {
+
+    It "Test deployment" {
+      New-AzResourceGroup -Name $testsResourceGroupName -Location "$testsResourceGroupLocation"
             (Test-AzResourceGroupDeployment -ResourceGroupName "$testsResourceGroupName" -TemplateFile "$templateFileLocation" -TemplateParameterObject $params).Count | Should -Not -BeGreaterThan 0
-        }
-        It "Deployment" {
-            $resultDeployment = New-AzResourceGroupDeployment -ResourceGroupName "$testsResourceGroupName" -TemplateFile "$templateFileLocation" -TemplateParameterObject $params
-            Write-Host ($resultDeployment | Format-Table | Out-String)
-            Write-Host ("Deployment state: " + $resultDeployment.ProvisioningState | Out-String)
-            $resultDeployment.ProvisioningState | Should -Be "Succeeded"
-        }
-        It "Search deployment" {
-            $result = Get-AzVM | Where-Object { $_.Name -like "$testsPrefix*" }
-            Write-Host ($result | Format-Table | Out-String)
-            $result | Should -Not -Be $null
-        }
     }
+    It "Deployment" {
+      $resultDeployment = New-AzResourceGroupDeployment -ResourceGroupName "$testsResourceGroupName" -TemplateFile "$templateFileLocation" -TemplateParameterObject $params
+      Write-Host ($resultDeployment | Format-Table | Out-String)
+      Write-Host ("Deployment state: " + $resultDeployment.ProvisioningState | Out-String)
+      $resultDeployment.ProvisioningState | Should -Be "Succeeded"
+    }
+    It "Search deployment" {
+      $result = Get-AzVM | Where-Object { $_.Name -like "$testsPrefix*" }
+      Write-Host ($result | Format-Table | Out-String)
+      $result | Should -Not -Be $null
+    }
+  }
 
-    Context 'Deployment test' {
+  Context 'Deployment test' {
 
-        BeforeAll {
-            $fgt = (Get-AzPublicIpAddress -Name $publicIPName -ResourceGroupName $testsResourceGroupName).IpAddress
-            Write-Host ("FortiGate public IP: " + $fgt)
-            $verify_commands = @'
+    BeforeAll {
+      $fgt = (Get-AzPublicIpAddress -Name $publicIPName -ResourceGroupName $testsResourceGroupName).IpAddress
+      Write-Host ("FortiGate public IP: " + $fgt)
+      $verify_commands = @'
             config system console
             set output standard
             end
@@ -179,32 +181,32 @@ Describe 'FGT A/A' {
             diag debug cloudinit show
             exit
 '@
-            $OFS = "`n"
-        }
-        It "FGT: Ports listening" {
-            ForEach( $port in $ports ) {
-                Write-Host ("Check port: $port" )
-                $portListening = (Test-Connection -TargetName $fgt -TCPPort $port -TimeoutSeconds 100)
-                $portListening | Should -Be $true
-            }
-        }
-        It "FGT A: Verify configuration" {
-            $result = $verify_commands | ssh -p 50030 -v -tt -i $sshkey -o StrictHostKeyChecking=no devops@$fgt
-            $LASTEXITCODE | Should -Be "0"
-            Write-Host ("FGT CLI info: " + $result) -Separator `n
-            $result | Should -Not -BeLike "*Command fail*"
-        }
-        It "FGT B: Verify configuration" {
-            $result = $verify_commands | ssh -p 50031 -v -tt -i $sshkey -o StrictHostKeyChecking=no devops@$fgt
-            $LASTEXITCODE | Should -Be "0"
-            Write-Host ("FGT CLI info: " + $result) -Separator `n
-            $result | Should -Not -BeLike "*Command fail*"
-        }
+      $OFS = "`n"
     }
+    It "FGT: Ports listening" {
+      ForEach ( $port in $ports ) {
+        Write-Host ("Check port: $port" )
+        $portListening = (Test-Connection -TargetName $fgt -TCPPort $port -TimeoutSeconds 100)
+        $portListening | Should -Be $true
+      }
+    }
+    It "FGT A: Verify configuration" {
+      $result = $verify_commands | ssh -p 50030 -v -tt -i $sshkey -o StrictHostKeyChecking=no devops@$fgt
+      $LASTEXITCODE | Should -Be "0"
+      Write-Host ("FGT CLI info: " + $result) -Separator `n
+      $result | Should -Not -BeLike "*Command fail*"
+    }
+    It "FGT B: Verify configuration" {
+      $result = $verify_commands | ssh -p 50031 -v -tt -i $sshkey -o StrictHostKeyChecking=no devops@$fgt
+      $LASTEXITCODE | Should -Be "0"
+      Write-Host ("FGT CLI info: " + $result) -Separator `n
+      $result | Should -Not -BeLike "*Command fail*"
+    }
+  }
 
-    Context 'Cleanup' {
-        It "Cleanup of deployment" {
-            Remove-AzResourceGroup -Name $testsResourceGroupName -Force
-        }
+  Context 'Cleanup' {
+    It "Cleanup of deployment" {
+      Remove-AzResourceGroup -Name $testsResourceGroupName -Force
     }
+  }
 }
