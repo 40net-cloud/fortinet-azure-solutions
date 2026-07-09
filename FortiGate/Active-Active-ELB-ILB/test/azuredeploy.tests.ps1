f19633b305050dbb3d6e9efe7da0d9f9f9d7feea
Joeri Van Hoof <jvanhoof@fortinet.com>
2026-07-09 15:23:18 +0200

    Update marketplace terms acceptance to specify offer type in tests

--- FortiGate/Active-Active-ELB-ILB/test/azuredeploy.tests.ps1
+++ FortiGate/Active-Active-ELB-ILB/test/azuredeploy.tests.ps1
@@ -250,10 +250,10 @@ Describe "FGT Active-Active ELB-ILB - $scenario" {
         }
 
         It 'Accepts the FortiGate marketplace image terms' {
-            $terms = Get-AzMarketplaceTerms -Publisher $imagePublisher -Product $imageOffer -Name $fortiGateImageSKU
+            $terms = Get-AzMarketplaceTerms -Publisher $imagePublisher -Product $imageOffer -Name $fortiGateImageSKU -OfferType 'virtualmachine'
             if (-not $terms.Accepted) {
                 Set-AzMarketplaceTerms -Publisher $imagePublisher -Product $imageOffer -Name $fortiGateImageSKU -Accept
-                $terms = Get-AzMarketplaceTerms -Publisher $imagePublisher -Product $imageOffer -Name $fortiGateImageSKU
+                $terms = Get-AzMarketplaceTerms -Publisher $imagePublisher -Product $imageOffer -Name $fortiGateImageSKU -OfferType 'virtualmachine'
             }
             $terms.Accepted | Should -Be $true
         }
